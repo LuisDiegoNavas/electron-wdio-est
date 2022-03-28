676d7f43a48088e556c42077edf59e8ec4fb716a
LuisDiegoNavas <luisdiegosn@hotmail.com>
2022-03-28 03:23:59 -0400

    Update application.spec.ts

--- test/application.spec.ts
+++ test/application.spec.ts
@@ -6,5 +6,10 @@ describe('Onbording', function () {
     const title = await browser.getTitle();
     expect(title).toEqual('Test');
     await browser.pause(4000)
+    const elem = await $('disabled-checkbox')
+    elem.click()
+    const elem2 = await $('.make-bigger')
+    elem2.click()
+    await browser.pause(2000)
   })
 })
